3994d9ee9b0c594c1889bb466e7d09b749876fe9
JolandaN <100078827+JolandaN@users.noreply.github.com>
2022-09-22 09:21:41 +0200

    index changed

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+
+// DBconfig establishes a connection with the database
+
+session_start();
+include_once ("DBconfig.php");
+include_once("header.php");
+
+
+if(isset($_GET["page"])) {
+    $page = $_GET["page"];
+} else {
+    $page = "login";
+}
+if($page) {
+    include("pages/".$page.".php");
+}
+?>
