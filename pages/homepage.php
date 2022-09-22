3bc8d34d6b3272deada85cf309ac4e333cfb920c
JolandaN <100078827+JolandaN@users.noreply.github.com>
2022-09-22 09:45:20 +0200

    videos was not for this assignment

--- pages/homepage.php
+++ pages/homepage.php
@@ -17,39 +17,7 @@
 
         <!-- this is homepage -->
 
-<body style="background-image: linear-gradient(to right top, #000000, #141414, #212121, #2f2f2f, #3d3d3d);   
-  background-position: center;
-  background-repeat: no-repeat;
-  background-attachment: fixed;">
-
-<?php
-if(!isset($_SESSION["ID"])&&$_SESSION["STATUS"]!="ACTIVE"){
-    echo "<script>
-    alert('You have no access to this page.');
-    location.href='../index.php';
-    </script>";
-}
-
-if(!empty($_POST['title'])) {
-    $sql = "SELECT * FROM video WHERE title = ?";
-    $stmt = $connection->prepare($sql);
-    $stmt->execute(array($_POST['title']));
-} else {
-    $sql = "SELECT * FROM video";
-    $stmt = $connection->prepare($sql);
-    $stmt->execute(array());
-}
-
-$videos = $stmt->fetchAll(PDO::FETCH_ASSOC);
-foreach($videos as $video) {
-    ?>
-    <iframe
-    style="margin-top: 5%; float: left; margin-left: 3%; border-radius: 10%;" 
-    width="280" height="157" src="<?php echo $video['link']?>" 
-        title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-        <?php
-}
-?>
+<body>
 
 <script src="https://unpkg.com/@popperjs/core@2.4.0/dist/umd/popper.min.js"></script>
 <script src="bootstrap-5.0.2-dist/bootstrap-5.0.2-dist/js/bootstrap.js"></script>
