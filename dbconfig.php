59393936d72be988c01790a71861f01c615387ae
JolandaN <100078827+JolandaN@users.noreply.github.com>
2022-09-22 09:19:38 +0200

    dbconfig// database

---
+++ dbconfig.php
@@ -0,0 +1,16 @@
+<?php
+
+// DBconfig establishes a connection with the database.
+
+DEFINE("USER","root");
+DEFINE("PASSWORD", "");
+try {
+    $connection = new
+    PDO("mysql:host=localhost;dbname=netfish",USER,PASSWORD);
+    $connection->setAttribute
+    (PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+}catch(PDOException $e) {
+    echo $e->getMessage();
+    echo "Could not connect.";
+}
+?>  
