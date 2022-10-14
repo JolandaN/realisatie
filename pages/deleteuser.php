6de96a67a281c900c7fa34186c0e8bccfb3feeee
JolandaN <100078827+JolandaN@users.noreply.github.com>
2022-10-14 11:51:14 +0200

    Delete profile possible for active/logged in user

---
+++ pages/deleteuser.php
@@ -0,0 +1,21 @@
+<?php 
+
+    // the php that is executed when using submit button in videos.php
+
+if(!isset($_SESSION["ID"])&&($_SESSION["STATUS"]!="ACTIVE")) {
+    echo "<script>
+    alert('You have no access to this page.');
+    location.href='../index.php';
+    </script>";
+}
+$sql = "DELETE FROM user WHERE ID = ?";
+$stmt = $connection->prepare($sql);
+try{
+    $stmt->execute(array($_POST['id']));
+    echo "<script>alert('User is deleted.');
+    location.href='index.php?page=changeprofile';
+    </script>";
+}catch(PDOException $e) {
+    echo $e->getMessage();
+}
+?>
