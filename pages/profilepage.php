2f08951b36060e36c326f1f98e11e2ec5ac291d5
JolandaN <100078827+JolandaN@users.noreply.github.com>
2022-11-03 11:02:35 +0100

    finally able to show user content from db

--- pages/profilepage.php
+++ pages/profilepage.php
@@ -1,76 +1,60 @@
+<?php 
+
+if(!isset($_SESSION["ID"])&&($_SESSION["STATUS"]!="ACTIVE")) {
+    echo "<script>
+    alert('You have no access to this page.');
+    location.href='../index.php';
+    </script>";
+}
+$sql = "SELECT * FROM user WHERE ID = :id";
+$stmt = $connection->prepare($sql);
+$stmt->execute([':id' => $_SESSION["USER_ID"]]);
+$users = $stmt->fetchAll(PDO::FETCH_ASSOC);
+// print_r($_SESSION["USER_ID"]);
+
+foreach ($users as $user) {
+    $id = $user["ID"];
+    $name = $user["name"];
+    $email = $user["email"];
+    
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta http-equiv="content-type"
-    content="text/html"; charset="utf-8">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Stattracker</title>
-    <link rel="stylesheet" href="bootstrap-5.0.2-dist/bootstrap-5.0.2-dist/css/bootstrap.css"/>
-    <script>
-        function logout() {
-            var loguit = confirm('Are you sure you want to logout?');
-            if (loguit) {
-                location.href='index.php?page=logout'
-            }
-        }
-    </script>
 </head>
-
-        <!-- this is profile -->
-
-        show profile content logged in user. 
-
-        <?php
-        if(!isset($_SESSION["ID"])&&$_SESSION["STATUS"]!="ACTIVE"){
-            echo "<script>
-            alert('You have no access to this page.');
-            location.href='../index.php';
-            </script>";
-        }
-/* Attempt MySQL server connection. Assuming you are running MySQL
-server with default setting (user 'root' with no password) */
-$link = mysqli_connect("localhost", "root", "", "stattracker");
- 
-// Check connection
-if($link === false){
-    die("ERROR: Could not connect. " . mysqli_connect_error());
-}
- 
-// Attempt select query execution
-$sql = "SELECT * FROM user WHERE ID = Can't get ID ";
-if($result = mysqli_query($link, $sql)){
-    if(mysqli_num_rows($result) > 0){
-        echo "<table>";
-            echo "<tr>";
-                echo "<th>ID</th>";
-                echo "<th>name</th>";
-                echo "<th>email</th>";
-                echo "<th>password</th>";
-            echo "</tr>";
-        while($row = mysqli_fetch_array($result)){
-            echo "<tr>";
-                echo "<td>" . $row['ID'] . "</td>";
-                echo "<td>" . $row['name'] . "</td>";
-                echo "<td>" . $row['email'] . "</td>";
-                echo "<td>" . $row['password'] . "</td>";
-            echo "</tr>";
-        }
-        echo "</table>";
-        // Free result set
-        mysqli_free_result($result);
-    } else{
-        echo "No records matching your query were found.";
-    }
-} else{
-    echo "ERROR: Could not able to execute $sql. " . mysqli_error($link);
-}
- 
-// Close connection
-mysqli_close($link);
-?>
-
 <body>
+ 
+<div class="container">
+        <h1 class='title'>Account</h1>
+        <br>
+            <table class="table table-dark">
+                <thead>
+                    <tr>
+                        <th>Account information</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td><?php echo 'Name: ' . $name; ?></td>
+                    </tr>
+                    <tr>
+                        <td><?php echo 'Email address: ' .
+                            $email ?></td>
+                    </tr>
 
-<script src="https://unpkg.com/@popperjs/core@2.4.0/dist/umd/popper.min.js"></script>
-<script src="bootstrap-5.0.2-dist/bootstrap-5.0.2-dist/js/bootstrap.js"></script>
+                    <tr><?php echo "<td><span class='right uppercase'> <a class='btn btn-primary' href='index.php?page=profile_edit&id=" .
+                            $user['ID'] .
+                            "'>Edit profile</a></span></td>"; ?>
+                </tbody>
+            </table>
+</div>
+
 </body>
-</html> 
+</html>
+
+<?php } ?>
