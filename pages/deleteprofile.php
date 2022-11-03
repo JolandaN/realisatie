c5bbfe7348e9487b8c3375c5ab0cb1d4e882b80e
JolandaN <100078827+JolandaN@users.noreply.github.com>
2022-11-03 15:48:34 +0100

    style changes,, sort of

--- pages/deleteprofile.php
+++ pages/deleteprofile.php
@@ -16,47 +16,30 @@ $stmt = $connection->prepare($sql);
 $stmt->execute([]);
 $users = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-
-
 ?>
-<div class="container d-flex justify-content-center">
-
-<div class="row my-3">
-    <div class="col-12">
-        <h1>Delete user</h1>
-
-    </div>
-</div>    
-
+        <h2>Delete user</h2>
 <?php foreach($users as $user) { ?>
             <!-- this form shows all users with the option to delete them from the database -->
 
-        <form name="edit" class="form"
+        <form name="delete" class="form"
         action="index.php?page=deleteuser"
         method="POST">
-                        <h6 class="h6 mb-1 fw-normal" >Name:</h6 >
-                        <input type="text" name="title" value="<?php echo $user['name'];?>" />  
+                        <p>Name:</p>
+                        <input type="text" name="name" value="<?php echo $user['name'];?>" />  
                         
-                        <h6 class="h6 mb-1 fw-normal" >Email:</h6>   
-                        <input type="text" name="genre" value="<?php echo $user['email'];?>" /> 
+                        <p>Email:</p>   
+                        <input type="text" name="email" value="<?php echo $user['email'];?>" /> 
                                                
                         <div class="icon_container">
                         <input type="submit" class="icon" name="delete" value="Delete"/>
 
                         <input type="hidden" name="id" value="<?php echo $user['ID'];?>" />
-                        <br>
-                        <br>
-                    <a href="index.php?page=homepage">Back</a>
-            <br>
-            <br>
         </form> 
-
-
 <?php
 }
 ?>
-
-</div>
+    <br><br>                
+    <a href="index.php?page=homepage">Back</a>
 <script src="https://unpkg.com/@popperjs/core@2.4.0/dist/umd/popper.min.js"></script>
 <script src="bootstrap-5.0.2-dist/bootstrap-5.0.2-dist/js/bootstrap.js"></script>
 </body>
