71b1ff14cb08e216f781199a554fb89a79656f97
JolandaN <100078827+JolandaN@users.noreply.github.com>
2022-11-03 12:18:20 +0100

    trying to display goals and assists on homepage

--- pages/homepage.php
+++ pages/homepage.php
@@ -19,9 +19,57 @@
 
         stats go here. 
 
+
+<?php
+
+$sql = "SELECT * FROM user WHERE ID = :id";
+$stmt = $connection->prepare($sql);
+$stmt->execute([':id' => $_SESSION["USER_ID"]]);
+$users = $stmt->fetchAll(PDO::FETCH_ASSOC);
+// print_r($_SESSION["USER_ID"]);
+
+foreach ($users as $user) {
+    $id = $user["ID"];
+    $name = $user["name"];
+    $goals = $user["goals"];
+    $assists = $user["assists"];
+
+?>
+
 <body>
+
+<div class="container">
+        <br>
+            <table class="table table-dark">
+                <thead>
+                    <tr>
+                        <h3>Goals and Assists</h3>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td><?php echo 'Name: ' . $name; ?></td>
+                    </tr>
+                    <tr>
+                        <td><?php echo 'Goals: ' .
+                            $goals ?></td>
+                    </tr>
+                    <tr>
+                        <td><?php echo 'Assists: ' .
+                            $assists ?></td>
+                    </tr>
+
+                    <tr>
+                        <?php echo "<td><span class='right uppercase'> <a class='btn btn-primary' href='index.php?page=profile_edit&id=" .
+                            $user['ID'] .
+                            "'>Edit profile</a></span></td>"; ?>
+                </tbody>
+            </table>
+</div>
 
 <script src="https://unpkg.com/@popperjs/core@2.4.0/dist/umd/popper.min.js"></script>
 <script src="bootstrap-5.0.2-dist/bootstrap-5.0.2-dist/js/bootstrap.js"></script>
 </body>
 </html> 
+
+<?php } ?>
