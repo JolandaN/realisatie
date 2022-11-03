266388e10c40be8b61caa8e0c21a8082e017bc17
JolandaN <100078827+JolandaN@users.noreply.github.com>
2022-11-03 13:30:27 +0100

    shows the stats per team and player

--- pages/statsperteam.php
+++ pages/statsperteam.php
@@ -1,48 +1,85 @@
+<?php 
+// Select all users from database where user is in the team
+$sql = 'SELECT * FROM user WHERE teamid = :id';
+$stmt = $connection->prepare($sql);
+$stmt->execute(['id' => $_GET['id']]);
+$users = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+$sql = 'SELECT * FROM team WHERE id = :id';
+$stmt = $connection->prepare($sql);
+$stmt->execute(['id' => $_GET['id']]);
+$team = $stmt->fetch(PDO::FETCH_ASSOC);
+
+$name = $team['teamname'];
+
+$goals = 'SELECT sum(`goals`) as total FROM user WHERE teamid=:id;';
+$stmt = $connection->prepare($goals);
+$stmt->execute(['id' => $_GET['id']]);
+$goals = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+$assists = 'SELECT sum(`assists`) as total FROM user WHERE teamid=:id;';
+$stmt = $connection->prepare($assists);
+$stmt->execute(['id' => $_GET['id']]);
+$assists = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta http-equiv="content-type"
-    content="text/html"; charset="utf-8">
-    <title>Stattracker</title>
-    <link rel="stylesheet" href="bootstrap-5.0.2-dist/bootstrap-5.0.2-dist/css/bootstrap.css"/>
-    <script>
-        function logout() {
-            var loguit = confirm('Are you sure you want to logout?');
-            if (loguit) {
-                location.href='index.php?page=logout'
-            }
-        }
-    </script>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="bootstrap/css/bootstrap.css">
+    <link rel="stylesheet" href="css/styles.css">
+    <script src="js/jquery-3.6.1.min.js" defer></script>
+    <script src="bootstrap/js/bootstrap.min.js" defer></script>
+    <title>Stat Tracker - <?php echo $name;
+    ?></title>
 </head>
-
-<?php
-
-    $totals = 'SELECT SUM(`goals`) AS totalGoals, SUM(`assists`) AS totalAssists FROM user';
-    $stmt = $connection->prepare($totals);
-    $stmt->execute();
-    $totals = $stmt->fetchAll(PDO::FETCH_ASSOC);
-?>
-
-
-
-
 <body>
+    <div class="container">
+        <!-- Shows Team Name -->
+        <h1 class='title'><?php echo $name ?></h1>
+        <div class="row">
+            <div class="col-6">
+                <!-- Shows total goals and assists -->
+                <h2 class="h2-title">Goals</h2>
+                <p class="h2-title"><?php foreach ($goals as $goal) {
+                    echo $goal['total'];
+                } ?></p>
+            </div>
+            <div class="col-6">
+                <h2 class="h2-title">Assists</h2>
+                <p class="h2-title"><?php foreach ($assists as $assist) {
+                    echo $assist['total'];
+                } ?></p>
+            </div>
+        </div>
 
-<div class="col-6">
-<!-- Shows total goals and assists -->
-        <h2 class="h2-title">Goals</h2>
-        <p class="h2-title"><?php foreach ($totals as $total) {
-            echo $total['totalGoals'];
-        ?></p>
+        <table class="table table-dark">
+            <thead>
+                <tr>
+                    <th>Player</th>
+                    <th>Goals</th>
+                    <th>Assists</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php foreach ($users as $user) {
+                    $id = $user['ID'];
+                    ?>
+                <tr>
+                    <!-- Display team member, goals and assist per team member -->
+                    <td><?php echo $user['name']; ?></td>
+                    <td><?php echo $user['goals']; ?></td>
+                    <td><?php echo $user['assists']; ?></td>
+                </tr>
+                <?php
+                
+                } ?>
+            </tbody>
+        </table>
     </div>
-    <div class="col-6">
-        <h2 class="h2-title">Assists</h2>
-        <p class="h2-title"><?php {
-            echo $total['totalAssists'];
-        } }?></p>
-</div>
-
-<script src="https://unpkg.com/@popperjs/core@2.4.0/dist/umd/popper.min.js"></script>
-<script src="bootstrap-5.0.2-dist/bootstrap-5.0.2-dist/js/bootstrap.js"></script>
 </body>
-</html> 
+</html>
